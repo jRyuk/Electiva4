b58c3dacbba184c5d38593b62c2bd5b9bfe6916d
jammsv <2559502015@mail.utec.edu.sv>
2019-11-12 19:55:46 -0600

    login

--- CuentasPorPagar/CuentasPorPagarWeb/Default.aspx.designer.cs
+++ CuentasPorPagar/CuentasPorPagarWeb/Default.aspx.designer.cs
@@ -13,6 +13,24 @@ namespace CuentasPorPagarWeb {
     public partial class _Default {
         
         /// <summary>
+        /// Control txtUser.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtUser;
+        
+        /// <summary>
+        /// Control txtPass.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPass;
+        
+        /// <summary>
         /// Control Button1.
         /// </summary>
         /// <remarks>
