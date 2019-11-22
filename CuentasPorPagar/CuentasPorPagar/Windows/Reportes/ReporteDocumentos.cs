9508057a9fd7dbd4594c28dcfe876a569bb5ccc7
jacobo.menjivar <jaco.menjivar@gmail.com>
2019-11-22 14:37:50 -0600

    Imprimir pdf

--- CuentasPorPagar/CuentasPorPagar/Windows/Reportes/ReporteDocumentos.cs
+++ CuentasPorPagar/CuentasPorPagar/Windows/Reportes/ReporteDocumentos.cs
@@ -1,5 +1,7 @@
 ﻿using DAL;
 using DAL.BAL;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 using Microsoft.Office.Interop.Excel;
 using System;
 using System.IO;
@@ -59,6 +61,60 @@ namespace CuentasPorPagar.Windows.Reportes
                 }
             }
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            var choofdlog = new FolderBrowserDialog();
+
+
+            if (!(choofdlog.ShowDialog() == DialogResult.OK))
+            {
+                return;
+            }
+            var folderPath = choofdlog.SelectedPath;
+
+
+            PdfPTable pdfTable = new PdfPTable(dataGridView1.ColumnCount);
+            pdfTable.DefaultCell.Padding = 3;
+            pdfTable.WidthPercentage = 90;
+            pdfTable.HorizontalAlignment = Element.ALIGN_LEFT;
+            pdfTable.DefaultCell.BorderWidth = 1;
+
+            //Adding Header row
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+            {
+                PdfPCell cell = new PdfPCell(new Phrase(column.HeaderText));
+               
+                pdfTable.AddCell(cell);
+            }
+
+            //Adding DataRow
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    pdfTable.AddCell(cell.Value == null  ? "" : cell.Value.ToString());
+                }
+            }
+
+            //Exporting to PDF
+        
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            using (FileStream stream = new FileStream(folderPath + "DataGridViewExport.pdf", FileMode.Create))
+            {
+                Document pdfDoc = new Document(PageSize.A2, 10f, 10f, 10f, 0f);
+                PdfWriter.GetInstance(pdfDoc, stream);
+                pdfDoc.Open();
+                pdfDoc.Add(pdfTable);
+                pdfDoc.Close();
+                stream.Close();
+            }
         }
     }
 }
